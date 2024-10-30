eb81d65b7c7fb3b4cbb7f55e0d695397c6f1a173
CoderRaaju <rbal7479@gmail.com>
2024-10-30 16:01:23 +0900

    LOL

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// Add any JavaScript interactions if necessary
+document.querySelector(".contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Form submitted successfully!");
+});
